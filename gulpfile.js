b2790564016a4c6bac778500552d86ed03774e6d
angelamaegarcia <angelamae.c.garcia@gmail.com>
2017-10-11 16:46:32 +0800

    fixed some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-// gulp.task('hello', function() {
-//   console.log('Hello Zell');
-// });
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/main.scss')
